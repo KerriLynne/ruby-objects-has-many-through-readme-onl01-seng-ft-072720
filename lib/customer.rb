7ef21c49ae87ba885ad03f59734e0389f052261a
Kerri L OGorman <kerri.l.ogorman@gmail.com>
2020-08-12 11:42:36 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,15 +1,15 @@
 class Customer
    attr_accessor :name, :age
 
-   @@all = []
+   @@all = []  #knows about all cusotmers
 
    def initialize(name, age)
      @name = name
      @age = age
-     @@all << self
+     @@all << self  #knows about all cusotmers
    end
 
-   def self.all
+   def self.all  #knows about all cusotmers
      @@all
    end
 
