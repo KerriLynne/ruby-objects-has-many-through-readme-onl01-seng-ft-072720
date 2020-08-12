ee9cf1e9ed781376849913e86bf450dcb37bb74a
Kerri L OGorman <kerri.l.ogorman@gmail.com>
2020-08-12 11:53:13 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,21 +1,21 @@
 class Customer
-   attr_accessor :name, :age # has a name and an age (can change)
+  attr_accessor :name, :age # has a name and an age (can change)
 
-   @@all = []  #knows about all cusotmers
+  @@all = []  #knows about all cusotmers
 
-   def initialize(name, age)
-     @name = name    #name and age are set upon initialization
-     @age = age
-     @@all << self  #knows about all cusotmers (tracks every instance of customer upon creation)
-   end
+  def initialize(name, age)
+    @name = name    #name and age are set upon initialization
+    @age = age
+    @@all << self  #knows about all cusotmers (tracks every instance of customer upon creation)
+  end
 
-   def self.all  #knows about all cusotmers
-     @@all
-   end
+  def self.all  #knows about all cusotmers
+    @@all
+  end
 
-   def new_meal(waiter, total, tip=0)
-     Meal.new(waiter, self, total, tip)
-   end    
+  def new_meal(waiter, total, tip=0)
+    Meal.new(waiter, self, total, tip)
+  end
 
 
 end
